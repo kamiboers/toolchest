b6bdd521eda7886b1e56201b03338879514b9f6c
Ashwin Rao <theonlyrao@gmail.com>
2016-04-23 21:57:32 -0600

    Test that only admins can see admin dashboard
    In this commit, three new tests exist to confirm that only admin users can see
    the admin dashboard.
    
    No additional functionality was added to controllers or models to make these
    tests pass.

--- spec/features/only_admin_can_see_admin_dashboard_spec.rb
+++ spec/features/only_admin_can_see_admin_dashboard_spec.rb
@@ -24,10 +24,15 @@ RSpec.feature "Admin has a dashboard" do
   end
 
   scenario "logged in user can't see it" do
+    user = create(:user)
+    login_user(user)
 
+    visit admin_dashboard_path
+    expect(page).to have_content "The page you were looking for doesn't exist (404)"
   end
 
   scenario "non-logged-in user can't see it" do
-
+    visit admin_dashboard_path
+    expect(page).to have_content "The page you were looking for doesn't exist (404)"    
   end
 end
